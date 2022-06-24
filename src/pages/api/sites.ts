c6a0572cfdcbb9b06a9503165c76c47dd766a304
luco17 <lucodonoghue@gmail.com>
2022-06-24 16:08:49 +0200

    added api route for sites

---
+++ src/pages/api/sites.ts
@@ -0,0 +1,13 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
+export default async (req, res) => {
+  const querySnapshot = await getDocs(collection(db, "sites"));
+  const sites = [];
+
+  querySnapshot.forEach((doc) => {
+    sites.push({ id: doc.id, ...doc.data() });
+  });
+
+  res.status(200).json(sites);
+};
